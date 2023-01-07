6bd5f201ed4f4a369160074568cac913eaa8fa9e
Tom Mason <tomma@airtm.io>
2023-01-06 19:54:33 -0500

    debug

--- renderers/commonRenderer.py
+++ renderers/commonRenderer.py
@@ -58,5 +58,5 @@ class CommonRenderer:
 
         # Add the logos to the image.
         # Logos will be bounded by the text region, and be centered vertically.
-        self.image.paste(awayLogo, (2, 8-(awayLogoHeight / 2)))
-        self.image.paste(homeLogo, (2, 24 - (homeLogoHeight / 2)))
+        self.image.paste(awayLogo, (2, 8 - math.floor(awayLogoHeight / 2)))
+        self.image.paste(homeLogo, (2, 24 - math.floor(homeLogoHeight / 2)))
