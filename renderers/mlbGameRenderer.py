2ba9a588966a20e2652435b5c120bfb1631f8681
Tom Mason <tomma@airtm.io>
2023-01-06 20:48:09 -0500

    debug

--- renderers/mlbGameRenderer.py
+++ renderers/mlbGameRenderer.py
@@ -39,7 +39,9 @@ class MlbGameRenderer(CommonRenderer):
         time = game['dateTime']['time'].split(':')
         ampm = game['dateTime']['ampm']
 
-        spacer = 5 if int(time[0]) > 9 else 1
+        time[0] = '12'
+
+        spacer = 6 if int(time[0]) > 9 else 1
 
         self.draw.text((self.firstMiddleCol+1, 0), f'{time[0]}', font=self.fontSmallReg, fill=self.fillWhite)
 
